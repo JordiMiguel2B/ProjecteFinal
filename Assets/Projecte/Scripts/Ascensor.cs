1f8d1819c7f3891c9cd1a4bd9761a0bba3b15755
Jordi <jordimiguel_bcnvj1mb1920@cev.com>
2020-10-22 18:24:19 +0200

    CambiosEscenario

---
+++ Assets/Projecte/Scripts/Ascensor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ascensor : MonoBehaviour
+{
+    public float movementSpeed = 7;
+    public bool estaPujant = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
